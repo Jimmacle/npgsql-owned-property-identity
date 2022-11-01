76365586f4f1739292b3f04a5ba1f40859a29c17
John Gross <john.gross@ul.com>
2022-11-01 16:07:26 -0500

    initial

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NpgsqlOwnedIdentityColumn;
+
+public class OwningEntity
+{
+    public int Id { get; set; }
+    
+    public OwnedEntity Owned { get; set; }
+}
+
+public class OwnedEntity
+{
+    public int ThisShouldBeIdentity { get; set; }
+}
+
+public class TestDbContext : DbContext
+{
+    public DbSet<OwningEntity> OwningEntities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var owning = modelBuilder.Entity<OwningEntity>();
+        owning.Property(x => x.Id).UseIdentityAlwaysColumn();
+        owning.OwnsOne(
+            x => x.Owned,
+            owned =>
+            {
+                owned.Property(x => x.ThisShouldBeIdentity)
+                    .UseIdentityAlwaysColumn();
+            });
+    }
+}
---
+++ Migrations/20221101210501_1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NpgsqlOwnedIdentityColumn.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OwningEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    Owned_ThisShouldBeIdentity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OwningEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OwningEntities");
+        }
+    }
+}
